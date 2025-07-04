0883df07cc5d03894cdd50547b307b0331c21010
Kaue2007-oss <gandrakaue7@gmail.com>
2025-07-04 11:46:29 -0300

    teu pai

--- src/main.jsx
+++ src/main.jsx
@@ -9,11 +9,10 @@ import Cadastro from '../routes/Cadastro.jsx'
 import MeuDia from '../routes/MeuDia.jsx'
 import Anotacoes from '../routes/Anotacoes.jsx'
 import Login from '../routes/Login.jsx'
-<<<<<<< HEAD
+
 import Importante from '../routes/Importante.jsx'
-=======
 import Eventos from '../routes/Eventos.jsx'
->>>>>>> fef3900ecfa5f76592e0c39baf5ad7b011b11704
+
 
 const router = createBrowserRouter([
   {
@@ -36,18 +35,16 @@ const router = createBrowserRouter([
     path:"Login",
     element:<Login/>
   },
-<<<<<<< HEAD
 {
   path:"Importante",
   element:<Importante/>
 },
-=======
+
   {
     path:"Eventos",
     element:<Eventos/>
   }
 
->>>>>>> fef3900ecfa5f76592e0c39baf5ad7b011b11704
   
 ])
 createRoot(document.getElementById('root')).render(
