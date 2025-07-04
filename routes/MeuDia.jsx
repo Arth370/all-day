18597010ba7558f8e16c51be2109a52b63e2f9a2
dudamonb <dudamonb@gmail.com>
2025-07-04 12:31:32 -0300

    Terceiro commit

--- routes/MeuDia.jsx
+++ routes/MeuDia.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import "../style/MeuDia.css"
 import sol_melhor from '../images/sol_melhor.png'
+import Icon from '../images/icon.png'
 import Circle from '../images/Circle.png'
+import Bloco from '../images/Bloco.png'
+import Refresh from '../images/Refresh.png'
+import loginImagem from '../images/login-people.png'
 import { Link } from 'react-router-dom'
 
 function MeuDia() {
@@ -12,30 +16,50 @@ function MeuDia() {
           <img src={sol_melhor} alt="Sol" className='sol' />
           <h1>Meu Dia</h1>
         </div>
-        <div>
-          <Link to='/'><p>Início</p></Link>
-          <Link to='/meu-dia'><p>Meu dia</p></Link>
-          <Link to='/Evento'><p>Eventos</p></Link>
-          <Link to='/Anotacoes'>Anotações</Link>
-
+        <div className='icons'>
+          <Link className='text-botao' to='/'><p>Início</p></Link>
+          <Link className='text-botao' to='/meu-dia'><p>Meu dia</p></Link>
+          <Link className='text-botao' to='/Eventos'><p>Eventos</p></Link>
+          <Link className='text-botao' to='/Anotacoes'>Anotações</Link>
         </div>
+
+        <div className='loginImagem'>
+          <div className='imagem-login'>
+          <img src={loginImagem} className='people'/>
+          </div>
+        </div>
+
       </header>
-      
-      <div className='meio'>
 
-        <div className='barrinhaTarefas'>
-        <img className="imagemzinha" src={Circle} alt="" />
-        <p className='mensagemTarefa'>Insira uma nova tarefa...</p>
+      <main>
+        <div id='tudo'>
+          <div id='barra'>
+            <div id='img'>
+              <img src={Circle} alt="Cicle" className='circle' />
+            </div>
+
+            <div>
+              <p id='Add'>Adicionar uma tarefa</p>
+            </div>
+          </div>
+
+          <div id='linha'>
+            <p>.</p>
+          </div>
+
+           <div id='barra2'>
+            <div id='img'>
+              <img src={Bloco} alt="Bloco" className='bloco' />
+              <img src={Refresh} alt="Refresh" className='refresh' />
+            </div>
+
+            <div id='criar'>
+              <p id='Cri'>Criar</p>
+            </div>
+          </div>
+
         </div>
-
-        <div className='barrinhaTarefas-Dois'>
-        <img className="imagemzinha" src={Circle} alt="" />
-        <p className='mensagemTarefa'>Insira uma nova tarefa...</p>
-        </div>
-
-      </div>
-      <footer></footer>
-
+      </main>
     </>
   )
 }
